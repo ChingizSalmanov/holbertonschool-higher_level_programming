0203a7b6fc629b0d127d6f6d7be5a9282f1d905d
ChingizSalmanov <13600@holbertonstudents.com>
2026-06-30 18:56:41 +0400

    task6: create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-word1 = "object-oriented programming is awesome"
-word2 = "with Python is fun"
-print(word1[:27] + " " + word2[:11])
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(str[39:67] + str[107:112] + str[0:6])
